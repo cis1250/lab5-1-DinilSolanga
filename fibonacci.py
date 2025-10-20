c60d620c7af7f6472dc73709717f8d897fbbcbff
Dins <129677977+DinilSolanga@users.noreply.github.com>
2025-10-20 12:26:23 -0400

    Implement Fibonacci sequence generator with input check
    Fix input validation and improve Fibonacci sequence function.

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,20 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+
+def fibonacci(terms,a,b):
+  for i in range(terms):
+    print(a, end=" ")
+    temp = a + b  
+    a = b          
+    b = temp  
+
+# Prompt the user for the number of terms.
+terms = int(input("How many terms of the fibonacci sequence would you like"))
+# Validate that the input is a positive integer.
+while (terms < 0 or terms != 0 or !isdigit(terms):
+  terms = int(input("Please enter a positive integer. "))
+a, b = 0, 1
+print("Fibonacci sequence:")    
+# Use a for loop to print the Fibonacci sequence up to that many terms.
+fibonacci(terms,a,b)
